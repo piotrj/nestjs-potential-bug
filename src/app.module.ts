6351817b9f256d5eb25258a8736b1a42b5c5fc1a
Piotr Jakubowski <piotrj@gmail.com>
2023-06-16 18:31:42 +0200

    No need for module2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module1 } from "./module1/module1.module";
-import { Module2 } from "./module2/module2.module";
 import { JobModule } from "./job/job.module";
 
 @Module({
-  imports: [Module1, Module2, JobModule],
+  imports: [Module1, JobModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/module2/module2.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Module2Service } from './module2.service';
-import { Module2Dependency } from './module2.dependency';
-
-@Module({
-  providers: [Module2Dependency, Module2Service],
-  exports: [Module2Service]
-})
-export class Module2 {}
